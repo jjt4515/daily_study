0f2af6b2bb6ea42f20bdfaf7671abc15fbe190c2
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-09-04 16:01:25 +0900

    스프링 기본

---
+++ demo/src/main/java/com/example/demo/order/AppConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.order;
+
+
+import com.example.demo.discount.DiscountPolicy;
+import com.example.demo.discount.FixDiscountPolicy;
+import com.example.demo.discount.RateDiscountPolicy;
+import com.example.demo.member.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+
+}
